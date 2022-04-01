e7718b94c6c25d66a93cc2fe1cb868ab6e2494ec
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-04-01 15:35:04 -0300

    Initial code of Assesment

---
+++ assesment/app/src/main/java/com/camelloncase/assesment/adapter/viewholder/RecipeViewHolder.kt
@@ -0,0 +1,20 @@
+package com.camelloncase.assesment.adapter.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.camelloncase.assesment.databinding.RecipeItemBinding
+import com.camelloncase.assesment.model.Recipe
+
+class RecipeViewHolder(private val itemBinding: RecipeItemBinding): RecyclerView.ViewHolder(itemBinding.root) {
+
+    val deleteButton = itemBinding.deleteImageView
+
+    fun bindItem(recipe: Recipe) {
+        itemBinding.nameTextView.text = recipe.recipeName
+        itemBinding.styleTextView.text = recipe.recipeStyle
+        itemBinding.createDateTextView.text = "Create: ${recipe.recipeCreateDate}"
+
+        if(recipe.recipeUpdateDate != null) {
+            itemBinding.updateDateTextView.text = "Update: ${recipe.recipeUpdateDate}"
+        }
+    }
+}
